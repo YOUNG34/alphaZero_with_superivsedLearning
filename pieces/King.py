9b22e6ab3c9e0d1d5fdbb8b572ecb33d1db8b38e
YOUNG34 <32638115+YOUNG34@users.noreply.github.com>
2018-06-23 16:10:47 +0100

    Add files via upload

---
+++ pieces/King.py
@@ -0,0 +1,6 @@
+class King(object):
+    def __init__(self):
+        self.init_location = {4:['K','w'],60:['K','b']}
+
+    def pieces(self):
+        return self.init_location
---
+++ pieces/Queen.py
@@ -0,0 +1,6 @@
+class Queen(object):
+    def __init__(self):
+        self.init_location = {3:['Q','w'],59:['Q','b']}
+
+    def pieces(self):
+        return self.init_location
