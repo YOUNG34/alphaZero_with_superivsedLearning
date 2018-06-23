41d5e1ed0688a1a4e73ccf074e1b1b7b4fbcb8be
YOUNG34 <32638115+YOUNG34@users.noreply.github.com>
2018-06-23 16:14:10 +0100

    Delete Knight.py

--- Knight.py
+++
@@ -1,6 +0,0 @@
-class Knight(object):
-    def __init__(self):
-        self.init_location = {1:['N','w'],6:['N','w'],57:['N','b'],62:['N','b']}
-
-    def pieces(self):
-        return self.init_location
