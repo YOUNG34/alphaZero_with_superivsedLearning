962814d02239586034888d959b4f61932ad6f570
YOUNG34 <32638115+YOUNG34@users.noreply.github.com>
2018-06-23 16:14:29 +0100

    Delete Rook.py

--- Rook.py
+++
@@ -1,7 +0,0 @@
-
-class Rook(object):
-    def __init__(self):
-        self.init_location = {0:['R','w'],7:['R','w'],63:['R','b'],56:['R','b']}
-
-    def pieces(self):
-        return self.init_location
