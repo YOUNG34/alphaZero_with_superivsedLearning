d2cd1a01d767c3046f8d069af64004585cb264d3
YOUNG34 <32638115+YOUNG34@users.noreply.github.com>
2018-06-23 16:13:57 +0100

    Delete Bishop.py

--- Bishop.py
+++
@@ -1,6 +0,0 @@
-class Bishop(object):
-    def __init__(self):
-        self.init_location = {2:['B','w'],5:['B','w'],58:['B','b'],61:['B','b']}
-
-    def pieces(self):
-        return self.init_location
