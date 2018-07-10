84f41fcdbd795cecb71513ae6864064c010a19fc
YOUNG34 <32638115+YOUNG34@users.noreply.github.com>
2018-07-10 21:19:40 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -3,6 +3,12 @@ import pygame
 import chess
 from mcts_pure import MCTSPlayer as mcts_pure
 from pygame.locals import *
+from tkinter import *
+from tkinter import messagebox
+
+
+
+
 
 class Settings(object):
     """docstring for Settings"""
@@ -10,7 +16,7 @@ class Settings(object):
         # initialize setting of game
 
         # screen setting
-        self.screen_width = 1200
+        self.screen_width = 2000
         self.screen_height = 1200
         self.bg_color = (230, 230, 230)
         self.position = [i for i in range(64)]
@@ -23,100 +29,196 @@ class Settings(object):
 
 
 
-def is_chess_clicked(position, event):
-    for each in position:
-        if (each.collidepoint(event.pos)):
-            return position.index(each)
-    return None
+class GUI(object):
+
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("hello world")
+        self.root.geometry('300x300')
+        l1 = Label(self.root, text="xls名：")
+        l1.pack()  # 这里的side可以赋值为LEFT  RTGHT TOP  BOTTOM
+        self.xls_text = StringVar()
+        xls = Entry(self.root, textvariable=self.xls_text)
+        self.xls_text.set(" ")
+        xls.pack()
+
+    def is_chess_clicked(self,position, event):
+
+        for each in position:
+            if (each.collidepoint(event.pos)):
+                return position.index(each)
+        return None
+
+    def on_click(self):
+        return self.xls_text.get()
 
 
-def run_game():
-    mcts_player = mcts_pure(c_puct=5, n_playout=1)
-    board = chess.Board()
-    pygame.init()
-    chess_sets = Settings()
-    screen = pygame.display.set_mode((chess_sets.screen_width, chess_sets.screen_height))
-    pygame.display.set_caption("Chess game")
+    def run_game(self):
 
-    background_color = (230,230,230)
-    image_path = '/home/k1758068/Desktop/alphaGoTest-master/image/'
-    chess_board = pygame.image.load(image_path + 'board_image.png').convert()
+        mcts_player = mcts_pure(c_puct=5, n_playout=5)
+        move_stack = ""
+        board = chess.Board()
 
-    black_b = pygame.image.load(image_path + 'blackb.png').convert_alpha()
-    black_k = pygame.image.load(image_path + 'blackk.png').convert_alpha()
-    black_n = pygame.image.load(image_path + 'blackn.png').convert_alpha()
-    black_p = pygame.image.load(image_path + 'blackp.png').convert_alpha()
-    black_q = pygame.image.load(image_path + 'blackq.png').convert_alpha()
-    black_r = pygame.image.load(image_path + 'blackr.png').convert_alpha()
+        chess_sets = Settings()
+        screen = pygame.display.set_mode((chess_sets.screen_width, chess_sets.screen_height))
+        pygame.display.set_caption("Chess Game")
 
-    white_b = pygame.image.load(image_path + 'whiteb.png').convert_alpha()
-    white_k = pygame.image.load(image_path + 'whitek.png').convert_alpha()
-    white_n = pygame.image.load(image_path + 'whiten.png').convert_alpha()
-    white_p = pygame.image.load(image_path + 'whitep.png').convert_alpha()
-    white_q = pygame.image.load(image_path + 'whiteq.png').convert_alpha()
-    white_r = pygame.image.load(image_path + 'whiter.png').convert_alpha()
+        pygame.init()
+        image_path = '/home/k1758068/Desktop/alphaGoTest-master/image/'
+        black_b = pygame.image.load(image_path + 'blackb.png').convert_alpha()
+        black_k = pygame.image.load(image_path + 'blackk.png').convert_alpha()
+        black_n = pygame.image.load(image_path + 'blackn.png').convert_alpha()
+        black_p = pygame.image.load(image_path + 'blackp.png').convert_alpha()
+        black_q = pygame.image.load(image_path + 'blackq.png').convert_alpha()
+        black_r = pygame.image.load(image_path + 'blackr.png').convert_alpha()
 
-    images = {3:[white_b, black_b], 6:[white_k, black_k], 2:[white_n, black_n],
-              1:[white_p, black_p], 5:[white_q, black_q], 4:[white_r, black_r]}
+        white_b = pygame.image.load(image_path + 'whiteb.png').convert_alpha()
+        white_k = pygame.image.load(image_path + 'whitek.png').convert_alpha()
+        white_n = pygame.image.load(image_path + 'whiten.png').convert_alpha()
+        white_p = pygame.image.load(image_path + 'whitep.png').convert_alpha()
+        white_q = pygame.image.load(image_path + 'whiteq.png').convert_alpha()
+        white_r = pygame.image.load(image_path + 'whiter.png').convert_alpha()
 
-    while True:
+        images = {3: [white_b, black_b], 6: [white_k, black_k], 2: [white_n, black_n],
+                       1: [white_p, black_p], 5: [white_q, black_q], 4: [white_r, black_r]}
 
-        screen.fill(background_color)
-        chess_board_x = 100
-        chess_board_y = 50
+        background_color = (230,230,230)
+        image_path = '/home/k1758068/Desktop/alphaGoTest-master/image/'
+        chess_board = pygame.image.load(image_path + 'board_image.png').convert()
 
 
-        screen.blit(chess_board,(chess_board_x,chess_board_y))
 
-        d = (950-55)/8
-        #piece_x, piece_y = 55+(j-1)*d, 55+(i-1)*d
+        while True:
 
-        for i in range(64):
-            if board.piece_at(i):
-                piece = board.piece_at(i).piece_type
-                color = board.piece_at(i).color
-                #print(piece,i)
-                if color:
-                    piece = images[piece][0]
+            screen.fill(background_color)
+            chess_board_x = 100
+            chess_board_y = 50
+
+
+            screen.blit(chess_board,(chess_board_x,chess_board_y))
+
+            d = (950-55)/8
+            #piece_x, piece_y = 55+(j-1)*d, 55+(i-1)*d
+
+            for i in range(64):
+                if board.piece_at(i):
+                    piece = board.piece_at(i).piece_type
+                    color = board.piece_at(i).color
+                    #print(piece,i)
+                    if color:
+                        piece = images[piece][0]
+                    else:
+                        piece = images[piece][1]
+
+                    x = 177 + (i % 8) * d
+                    y = 23 + (8 - (i // 8))*d
+                    screen.blit(piece, (x, y))
+            myfont = pygame.font.SysFont('test', 30)
+            textsurface = myfont.render(move_stack, True, (0, 0, 0))
+            screen.blit(textsurface, (1000, 1000))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                #try:
+                if board.turn == True:
+                    if event.type == MOUSEBUTTONDOWN:
+                        if event.button == 1:
+                            selected_position = self.is_chess_clicked(chess_sets.position, event)
+                            select_piece = board.piece_at(selected_position)
+
+                            if select_piece.color == True:
+                                from_position = selected_position
+                                print(from_position)
+                            else:
+                                pass
+
+
+                        if event.button == 3:
+                            selected_position = self.is_chess_clicked(chess_sets.position, event)
+                            to_position = selected_position
+                            a = [48,49,50,51,52,53,54,55]
+
+                            # The promotion precess of PAWN pieces
+                            if from_position in a and select_piece.piece_type == 1:
+                                a = Button(self.root, text="press", command=self.on_click).pack()
+                                print(a)
+                                self.root.mainloop()
+
+                                move = chess.Move(from_position, to_position, promotion = a)
+                                print(move)
+                            else:
+                                move = chess.Move(from_position, to_position)
+
+                            # Move check and make move
+                            if move not in board.legal_moves:
+                                print("invalide move!",move,board.legal_moves)
+                            else:
+                                board.push(move)
+                                move_stack += "," + board.peek().uci()
+                                myfont = pygame.font.SysFont('test',30)
+
+                                textsurface = myfont.render(move_stack,True,(0,0,0))
+                                screen.blit(textsurface,(100,100))
+
+
                 else:
-                    piece = images[piece][1]
+                    if event.type == MOUSEBUTTONDOWN:
+                        if event.button == 1:
+                            selected_position = self.is_chess_clicked(chess_sets.position, event)
+                            select_piece = board.piece_at(selected_position)
 
-                x = 177 + (i % 8) * d
-                y = 23 + (8 - (i // 8))*d
-                screen.blit(piece, (x, y))
+                            if select_piece.color == False:
+                                from_position = selected_position
+                            else:
+                                pass
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                        if event.button == 3:
+                            selected_position = self.is_chess_clicked(chess_sets.position, event)
+                            to_position = selected_position
+                            a = [48,49,50,51,52,53,54,55]
+                            b = [56,57,58,59,60,61,62,63]
+                            if from_position in a and select_piece == 1:
+                                move = chess.Move(from_position, to_position, promotion=2, drop=None)
+                                print(move)
+                            else:
+                                move = chess.Move(from_position, to_position)
+                            if move not in board.legal_moves:
+                                print("invalide move!", move,from_position, board.legal_moves)
+                            else:
+                                board.push(chess.Move(from_position, to_position))
+                                move_stack += "," + board.peek().uci()
+                                myfont = pygame.font.SysFont('test', 30)
+                                textsurface = myfont.render(move_stack, True, (0, 0, 0))
+                                screen.blit(textsurface, (100, 100))
 
-            if board.turn == True:
-                if event.type == MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        selected_position = is_chess_clicked(chess_sets.position, event)
-                        select_piece = board.piece_at(selected_position)
+                # else:
+                #     move = mcts_player.get_action(board)
+                #     board.push(chess.Move.from_uci(str(move)))
+                #     move_stack += "," + board.peek().uci()
+                #     print(move_stack)
+                #     myfont = pygame.font.SysFont('test', 30)
+                #     textsurface = myfont.render(move_stack, True, (0, 0, 0))
+                #     screen.blit(textsurface, (100, 100))
 
-                        if select_piece.color == True:
-                            from_position = selected_position
-                            print(from_position)
-                        else:
-                            pass
 
-                    if event.button == 3:
-                        selected_position = is_chess_clicked(chess_sets.position, event)
-                        to_position = selected_position
-                        move = chess.Move(from_position, to_position)
-                        if move not in board.legal_moves:
-                            print("invalide move!")
-                        else:
-                            board.push(chess.Move(from_position, to_position))
 
-            else:
-                move = mcts_player.get_action(board)
-                board.push(chess.Move.from_uci(str(move)))
 
-        pygame.display.flip()
+                if board.is_variant_end():
+                    if board.turn:
+                        print("winner is : White side")
+                    else:
+                        print("winner is : Black side")
+                # except:
+                #     print("Error")
+                #     continue
+
+
+
+            pygame.display.flip()
 
 
 if __name__ == '__main__':
-    run_game()
+    run = GUI()
+    run.run_game()
