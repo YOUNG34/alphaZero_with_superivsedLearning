6a4668c06eba692924ca98b5a0683f5b22f0eba1
YOUNG34 <32638115+YOUNG34@users.noreply.github.com>
2018-08-22 18:09:35 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,175 @@
+import os, sys
+sys.path.append(os.path.join('..', 'src'))
+from agent.player_chess import ChessPlayer
+from config import Config, PlayWithHumanConfig
+from env.chess_env import ChessEnv
+import chess
+import sys
+import pygame
+import chess
+#from mcts_pure import MCTSPlayer as mcts_pure
+from pygame.locals import *
+from tkinter import *
+import pop_dialog
+import os, sys
+sys.path.append(os.path.join('..', 'src'))
+from agent.player_chess import ChessPlayer
+from config import Config, PlayWithHumanConfig
+from env.chess_env import ChessEnv
+
+
+class Settings(object):
+    """docstring for Settings"""
+    def __init__(self):
+        # initialize setting of game
+
+        # screen setting
+        self.screen_width = 1200
+        self.screen_height = 1200
+        self.bg_color = (230, 230, 230)
+        self.position = [i for i in range(64)]
+        self.from_position = None
+        k = 0
+        for i in range(7,-1,-1):
+            for j in range(8):
+                self.position[k] = pygame.Rect(148+j*111.75, 100+i*111.75, 111.75, 111.75)
+                k+=1
+
+class GUI(object):
+    def get_player(self, config):
+        from src.chess_zero.agent.model_chess import ChessModel
+        from src.chess_zero.lib.model_helper import load_best_model_weight
+        model = ChessModel(config)
+        if not load_best_model_weight(model):
+            raise RuntimeError("Best model not found!")
+        return ChessPlayer(config, model.get_pipes(config.play.search_threads))
+
+    def info(self, depth, move, score):
+        print(f"info score cp {int(score*100)} depth {depth} pv {move}")
+        sys.stdout.flush()
+
+    def is_chess_clicked(self,position, event):
+
+        for each in position:
+            if (each.collidepoint(event.pos)):
+                return position.index(each)
+        return None
+
+
+    def run_game(self):
+        default_config = Config()
+        PlayWithHumanConfig().update_play_config(default_config.play)
+        me_player = None
+        env = ChessEnv().reset()
+
+        #mcts_player = mcts_pure(c_puct=5, n_playout=5)
+        move_stack = ""
+        board = chess.Board()
+
+        chess_sets = Settings()
+        screen = pygame.display.set_mode((chess_sets.screen_width, chess_sets.screen_height))
+        pygame.display.set_caption("Chess Game")
+
+        pygame.init()
+        image_path = '/home/k1758068/Desktop/alphaGoTest-master/image/'
+        black_b = pygame.image.load(image_path + 'blackb.png').convert_alpha()
+        black_k = pygame.image.load(image_path + 'blackk.png').convert_alpha()
+        black_n = pygame.image.load(image_path + 'blackn.png').convert_alpha()
+        black_p = pygame.image.load(image_path + 'blackp.png').convert_alpha()
+        black_q = pygame.image.load(image_path + 'blackq.png').convert_alpha()
+        black_r = pygame.image.load(image_path + 'blackr.png').convert_alpha()
+
+        white_b = pygame.image.load(image_path + 'whiteb.png').convert_alpha()
+        white_k = pygame.image.load(image_path + 'whitek.png').convert_alpha()
+        white_n = pygame.image.load(image_path + 'whiten.png').convert_alpha()
+        white_p = pygame.image.load(image_path + 'whitep.png').convert_alpha()
+        white_q = pygame.image.load(image_path + 'whiteq.png').convert_alpha()
+        white_r = pygame.image.load(image_path + 'whiter.png').convert_alpha()
+
+        images = {3: [white_b, black_b], 6: [white_k, black_k], 2: [white_n, black_n],
+                       1: [white_p, black_p], 5: [white_q, black_q], 4: [white_r, black_r]}
+
+        background_color = (230,230,230)
+        #image_path = '/Desktop/alphaGoTest-master/image/'
+        chess_board = pygame.image.load(image_path + 'board_image.png').convert()
+        while True:
+
+            screen.fill(background_color)
+            chess_board_x = 100
+            chess_board_y = 50
+
+            screen.blit(chess_board, (chess_board_x, chess_board_y))
+
+            d = (950 - 55) / 8
+            for i in range(64):
+                if board.piece_at(i):
+                    piece = board.piece_at(i).piece_type
+                    color = board.piece_at(i).color
+
+                    if color:
+                        piece = images[piece][0]
+                    else:
+                        piece = images[piece][1]
+
+                    x = 177 + (i % 8) * d
+                    y = 23 + (8 - (i // 8)) * d
+                    screen.blit(piece, (x, y))
+            myfont = pygame.font.SysFont('test', 30)
+            textsurface = myfont.render(move_stack, True, (0, 0, 0))
+            screen.blit(textsurface, (1000, 1000))
+            for event in pygame.event.get():
+                if board.turn == True:
+                    if event.type == MOUSEBUTTONDOWN:
+                        if event.button == 1:
+                            selected_position = self.is_chess_clicked(chess_sets.position, event)
+                            select_piece = board.piece_at(selected_position)
+
+                            if select_piece.color == True:
+                                from_position = selected_position
+                                print(from_position)
+                            else:
+                                pass
+
+                        if event.button == 3:
+                            selected_position = self.is_chess_clicked(chess_sets.position, event)
+                            to_position = selected_position
+                            a = [48, 49, 50, 51, 52, 53, 54, 55]
+                            # The promotion precess of PAWN pieces
+                            if from_position in a and select_piece.piece_type == 1:
+                                # a = self.create_dialog()#
+                                a = pop_dialog.main()
+                                move = chess.Move(from_position, to_position, promotion=int(a), drop=None)
+                                print(int(a), move)
+                            else:
+                                move = chess.Move(from_position, to_position)
+
+                            # Move check and make move
+                            if move not in board.legal_moves:
+                                print("invalide move!", move, board.legal_moves)
+                            else:
+                                board.push(move)
+                                move_stack += "," + board.peek().uci()
+                                myfont = pygame.font.SysFont('test', 30)
+
+                                textsurface = myfont.render(move_stack, True, (0, 0, 0))
+                                screen.blit(textsurface, (100, 100))
+                else:
+                    me_player = self.get_player(default_config)
+
+                    action = me_player.action(env, False)
+                    env.step(action)
+    
+                    screen.blit(textsurface, (100, 100))
+
+                    me_player = self.get_player(default_config)
+                    action = me_player.action(env, False)
+                    print(f"bestmove {action}")
+                    board.push(chess.Move.from_uci(action))
+
+            pygame.display.flip()
+
+
+
+if __name__ == '__main__':
+    run = GUI()
+    run.run_game()
